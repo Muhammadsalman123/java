f55adce575e6857b53edec1f842394aa5af70c16
unknown <mrsallu881@gmail.com>
2021-07-17 16:15:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+
+
+//only msg show
+//alert('assalam o aliakom')
+//input lyne k liye prompt use krte hai.
+///prompt('enter value','01')
+//situation check
+//confirm('yes / noo')
+//srf console may check krna/
+//console.log('heYY')
+
+// var $data = "ABC"
+// var $data1 = 12
+
+// console.log($data*$data1)
+// console.log($data/$data1)
+// console.log($data+$data1)
+// console.log($data/$data1)
+
+
+// var phele = 5
+// var dosri = 6
+// var total = phele + dosri ;
+// var total1 = phele - dosri ;
+// var total2 = phele / dosri ;
+// var total3 = phele * dosri ;
+
+// document.write(total) 
+// document.write(total1)
+// document.write(total2)
+// document.write(total3)
+
+// a = prompt("enter vale : ")
+// console.log(a)
+// console.log(typeof(a))
+
+// a = prompt("enter vale : ")
+// b = prompt("enter vale : ")
+// c = a + b 
+//  console.log(c)
+// console.log(typeof(c))
+
+
+// leap = +prompt("enter value : ")
+
+// if(leap%4 == 0){
+//     console.log(leap+" this is leap year")
+// }else{
+//     console.log(leap+" this is not leap year")
+
+// }
+// evenodd = +prompt("enter value : ")
+
+// if(evenodd%4 == 0){
+//     console.log(evenodd+" this is Even number")
+// }else{
+//     console.log(evenodd+" this is  odd number")
+
+// }
+
+
+
+
+
+a = eval(prompt("enter value 1 : "))
+
+b = eval(prompt("enter value 2 : "))
+
+operator = prompt("enter operator : ");
+
+
+if(operator=="+"){
+alert(a+b);
+}
+else if(operator=="-"){
+    alert(a-b);
+    }
+
+    else if(operator=="*"){
+        alert(a*b);
+        }
+        
+        else if(operator=="/"){
+            alert(a/b);
+            }
+            else{
+                alert("sorry")
+            }
+
